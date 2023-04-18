1240d2573b505930c792a50f8ea645d387dd8726
deMartiniFrancesco <18606@studenti.marconiverona.edu.it>
2023-04-18 13:14:38 +0200

    Update graphic
    frontend

--- Frontend/app/app.py
+++ Frontend/app/app.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    dati = [{'data': '01/01/2021', 'pm10': 20, 'pm25': 10},
-    {'data': '02/01/2021', 'pm10': 18, 'pm25': 7},
-    {'data': '03/01/2021', 'pm10': 24, 'pm25': 11},
-    {'data': '04/01/2021', 'pm10': 14, 'pm25': 8},
-    {'data': '05/01/2021', 'pm10': 12, 'pm25': 6}]
+    dati = []
+    try:
+        connection = mysql.connector.connect(host='eco_monitor_mysql',
+                                             user='root',
+                                             database='edciv',
+                                             password='passwordsicura')
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            print("Connected to MySQL Server version ", db_Info)
+            cursor = connection.cursor()
+            cursor.execute("select database()", "")
+            record = cursor.fetchone()
+            print("You're connected to database: ", record)
+            print("Performing queries...")
+            # query
+            sql = "SELECT * FROM rilevazioni;"
+            cursor.execute(sql)
+            dati = cursor.fetchall()
+
+            # close connection
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+
     return render_template('index.html', dati=dati)
 
 @app.route('/about')
