ebade96df5ef4bad94af14313cf4a6da356b6485
4BI-SecchiPietro-2021-2022 <19168@studenti.marconiverona.edu.it>
2023-05-07 19:26:43 +0200

    added graph

--- Frontend/app/static/js/graph.js
+++ Frontend/app/static/js/graph.js
@@ -1,54 +1,170 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-
-function generateRandomData(numPoints) {
-  var data = [];
-  for (var i = 0; i < numPoints; i++) {
-    data.push(Math.floor(Math.random() * 100) + 1);
-  }
-  return data;
+function fetchData(station, pollutant) {
+    return fetch(`/api/v1/stations/${station}/pollutants/${pollutant}`)
+        .then(response => response.json())
+        .then(data => {
+            const labels = data.map(item => item.data);
+            const values = data.map(item => item.valore);
+            return { labels, values };
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
-var myChart = new Chart("myChart", {
-    type: 'bar',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October'],
-        datasets: [{
-            label: 'Random Data',
-            data: generateRandomData(10),
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+function createGraph(labels, values) {
+    if(window.myChart instanceof Chart)
+    {
+        window.myChart.destroy();
     }
+    const ctx = document.getElementById('myChart').getContext('2d');
+
+    window.myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Value',
+                data: values,
+                backgroundColor: 'transparent',
+                borderColor: 'white',
+                borderWidth: 2,
+                pointBackgroundColor: 'white'
+            }]
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            responsive: true,
+            maintainAspectRatio: false,
+            title: {
+                display: false,
+                text: 'Line Graph'
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Date'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value'
+                    },
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+
+}
+
+function fetchPollutants(station) {
+    return fetch(`/api/v1/stations/${station}/pollutants`)
+        .then(response => response.json())
+
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+function fetchStations() {
+    return fetch(`/api/v1/stations`)
+        .then(response => response.json())
+        .then(data => data.map(item => item.nome))
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
+function displayGraph(station, pollutant) {
+    fetchData(station, pollutant)
+        .then(({ labels, values }) => createGraph(labels, values));
+}
+
+function populateStations() {
+    const stationsDropdown = document.getElementById('stations');
+    fetchStations().then(stations => {
+        stations.forEach(station => {
+            const option = document.createElement('option');
+            option.text = station;
+            option.value = station;
+            stationsDropdown.add(option);
+        });
+        stationsDropdown.disabled = false;
+    }).catch(error => console.error(error));
+}
+
+// Fetch available pollutants for the selected station and populate the dropdown menu
+function populatePollutants() {
+    const stationName = document.getElementById('stations').value;
+    const pollutantsDropdown = document.getElementById('pollutants');
+    fetchPollutants(stationName).then(pollutants => {
+        pollutants.forEach(pollutant => {
+            const option = document.createElement('option');
+            option.text = pollutant;
+            option.value = pollutant;
+            pollutantsDropdown.add(option);
+        });
+        pollutantsDropdown.disabled = false;
+    }).catch(error => console.error(error));
+}
+
+
+document.getElementById('stations').addEventListener('change', () => {
+    document.getElementById('pollutants').disabled = true; // disable pollutants dropdown until station is selected
+    const stationName = document.getElementById('stations').value;
+    fetchPollutants(stationName) // get pollutants for selected station
+        .then(pollutants => {
+            const pollutantsDropdown = document.getElementById('pollutants');
+            pollutantsDropdown.innerHTML = ''; // clear previous options
+            let firstPollutant;
+            pollutants.forEach(pollutant => {
+                const option = document.createElement('option');
+                option.value = pollutant;
+                option.text = pollutant;
+                pollutantsDropdown.appendChild(option);
+                if (!firstPollutant) {
+                    firstPollutant = pollutant;
+                }
+            });
+            pollutantsDropdown.disabled = false; // enable pollutants dropdown
+            displayGraph(stationName, firstPollutant); // update graph with selected station and pollutant
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 });
+
+document.getElementById('pollutants').addEventListener('change', () => {
+    const stationName = document.getElementById('stations').value;
+    const pollutantName = document.getElementById('pollutants').value;
+    displayGraph(stationName, pollutantName); // update graph with selected station and pollutant
+});
+
+// initial call to get available stations and populate dropdown
+fetchStations()
+    .then(stations => {
+        const stationsDropdown = document.getElementById('stations');
+        stations.forEach(station => {
+            const option = document.createElement('option');
+            option.value = station;
+            option.text = station;
+            stationsDropdown.appendChild(option);
+        });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+
+
+
+
+
+
